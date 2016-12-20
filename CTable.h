00fcedde5fd4069f9e729662c2062f583ac9ec5d
mahcinek <marcin.piwek@gmail.com>
2016-12-20 16:19:21 +0100

    lista 1+6

--- CTable.h
+++ CTable.h
@@ -8,29 +8,111 @@
 #include <iostream>
 
 using namespace std;
-
+template <class T>
 class CTable {
 private:
     string s_name;
-    int* i_Table;
+    T *i_Table;
     int table_length;
     int DEFAULT_VALUE = -1;
 public:
-    CTable();
-    CTable(string s_name);
-    CTable(const CTable &pcOther);
-    string getName();
-    void setName(string s_name);
-    //string getFullInfo();
-    void setTableLength(int table_length);
-    bool setValue(int value, int place);
-    bool getValue(int *ptr_value, int place)const;
-    int getTableLength();
-    bool clear (int iPos);
-    bool del();
-    bool setSize(int iSize);
-    ~CTable();
-};
+    CTable(){
+        table_length =5;
+        this->s_name = "Tablica";
+        i_Table = new T[5];
+       //std::cout << "bezp:" <<s_name <<endl;
+    };
+    CTable(int size, string s_name)
+    {
+        this->table_length=size;
+        this->s_name=s_name;
+        i_Table = new T[size];
+    }
+    CTable (int size,string s_name,T defValue){
+        this->table_length=size;
+        this->s_name=s_name;
+        i_Table = new T[size];
+        for (int i=0;i<table_length;i++)
+            i_Table[i]=defValue;
+    };
+    CTable(string s_name)
+    {
+        this->s_name = s_name;
+        std::cout << "parametr:" << s_name <<endl;
+    };
+    CTable(const CTable &pcOther){
+        s_name = pcOther.s_name+"_copy";
+        table_length = pcOther.table_length;
+        i_Table = new int[table_length];
 
+        int tmp_val = 0;
+        for (int i = 0; i < table_length; i++)
+        {
+            tmp_val=pcOther.getValue(i);
+            i_Table[i] = tmp_val;
+        }
+        cout << "kopiuj: " << pcOther.s_name<<endl;
+    };
+    string getName(){
+        return s_name;
+    };
+    void setName(string s_name){
+        this->s_name = s_name;
+    };
+    //string getFullInfo();
+    void setTableLength(int table_length) {
+        delete[] i_Table;
+        i_Table = new int[table_length];
+        this->table_length = table_length;
+
+    };
+    bool setValue(T value, int place){
+        if (place >= table_length||place<0)
+            throw -1;
+        else
+        {
+            i_Table[place] = value;
+            return true;
+        }
+
+    };
+    T getValue(int place)const
+    {
+        if (place >= table_length||place<0)
+            throw -1;
+        else
+        {
+            return  i_Table[place];
+
+        }
+    };
+    int getTableLength(){
+        return table_length;
+    };
+    bool clear (int iPos){
+        for (int i=0;i<table_length;i++)
+            i_Table[i]=DEFAULT_VALUE;
+    };
+    bool del();
+    bool setSize(int iSize){
+        int* pomTable=new int [iSize];
+        for (int i=0;i<iSize||i<table_length;i++)
+        {
+            pomTable[i]=i_Table[i];
+        }
+        if (iSize>table_length)
+        {
+            for (int i=table_length;i<iSize;i++)
+                pomTable[i]= DEFAULT_VALUE;
+        }
+        int * pomTablePointer=i_Table;
+        i_Table=pomTable;
+        delete[](pomTablePointer);
+    };
+    ~CTable(){
+       // cout << "usuwam:" << s_name;
+        delete[] i_Table;
+    };
+};
 
 #endif //LISTA1_CTABLE_H
--- main.cpp
+++ main.cpp
@@ -1,179 +1,46 @@
-#include <iostream>
 #include <sstream>
 #include <vector>
 #include <cstdlib>
 #include <iterator>
+#include <stdlib.h>
 #include "CTable.h"
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include "string.h"
+#include "Osoba.h"
+#include <cstdlib>
+
 
 
 int main() {
     using namespace std;
-    vector<string> vstrings;
-
-    int iVectorSize;
-    cout<<"podaj ilosc CTabel"<<endl;
-    cin>>iVectorSize;
-    vector<CTable *> vCtable (iVectorSize, nullptr);
-    //int a=1;
-    //for (int i=0;i<iVectorSize;i++)
-     //vCtable.push_back(nullptr);
-   // string sCommnad=vstrings[0];
-
-
-    //if (vstrings.size()==3)
-    //{
-      // iVal2=atoi(vstrings[2].c_str());
-    //}
-    bool menu = true;
-    while (menu) {
-        string command;
-        cout << "Podaj komende:" << endl;
-        std::cin.sync();
-        std::getline( std::cin, command );
-        int iVal1 = -1;
-        int iVal2 = -1;
-//        istringstream iss(command);
-//        string s;
-        stringstream ss(command);
-        istream_iterator<string> begin(ss);
-        istream_iterator<string> end;
-        vector<string> vstrings(begin, end);
-        copy(vstrings.begin(), vstrings.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-        string sCommnad = vstrings[0];
-
-
-        if (sCommnad == "createDef") {
-            if (vstrings.size()>=1)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize)
-                    vCtable[iVal1]= new CTable ();
-                    else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;
-            }
-            else if (sCommnad == "create") {
-            if (vstrings.size()>=3)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize){
-                    vCtable[iVal1]= new CTable (vstrings[3]);
-                    vCtable[iVal1]->setTableLength(atoi(vstrings[2].c_str()));}
-                else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;
-            }
-            else if (sCommnad == "createCopy") {
-            if (vstrings.size()>=1)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            iVal2 = atoi(vstrings[2].c_str());
-            if (vCtable[iVal2]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize){
-                    vCtable[iVal1]= new CTable ( *vCtable[iVal2]);}
-                else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;
-        }
-            else if (sCommnad == "setValue") {
-            if (vstrings.size()>=2)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize){
-                    iVal2 = atoi(vstrings[2].c_str());
-                    vCtable[iVal1]->setValue(atoi(vstrings[3].c_str()),iVal2);
-                }
-                else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;
-        }
-            else if (sCommnad == "getValue") {if (vstrings.size()>=2)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize){
-                    iVal2 = atoi(vstrings[2].c_str());
-                    int iOdp;
-                    vCtable[iVal1]->getValue(&iOdp,iVal2);
-                    cout<<"Wynik: "<<iOdp<<endl;
-                }
-                else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;}
-            else if (sCommnad == "setName") {if (vstrings.size()>=2)
-            { iVal1 = atoi(vstrings[1].c_str()); }
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            if (iVal1 >= 0) {
-                if (iVal1 < iVectorSize){
-                    vCtable[iVal1]->setName(vstrings[2]);
-                }
-                else cout<<"Nieprawidlowe miejsce"<<endl;}
-            vstrings.clear();
-            continue;}
-            else if (sCommnad == "getName") {
-
-            if (vstrings.size() >= 1)
-                    iVal1 = atoi(vstrings[1].c_str());
-            if(vCtable[iVal1]== nullptr) {cout<<"Nullowy obiekt"<<endl;
-                continue;}
-                cout <<"Nazwa to:" <<vCtable[iVal1]->getName()<<endl;
-            }
-        else if (sCommnad == "delete") {
-            if (vstrings.size() >= 1)
-                iVal1 = atoi(vstrings[1].c_str());
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            cout << vCtable[iVal1]->del()<<endl;
-            vCtable[iVal1]= nullptr;
-            continue;
-        }
-        else if (sCommnad == "clear") {
-            if (vstrings.size() >= 1)
-                iVal1 = atoi(vstrings[1].c_str());
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            cout << vCtable[iVal1]->clear(iVal1);
-            continue;
-        }
-        else if (sCommnad == "setSize") {
-            if (vstrings.size() >= 1)
-                iVal1 = atoi(vstrings[1].c_str());
-            if (vCtable[iVal1]== nullptr)
-            {
-                cout<<"Nie ma takiego obiektu"<<endl;
-                continue;
-            }
-            iVal2=atoi(vstrings[2].c_str());
-            cout << vCtable[iVal1]->setSize(iVal2)<<endl;
-            continue;
-        }
-        else if (sCommnad == "exit") { menu = false; }
-            else { cout << "Bledna komenda"<<endl; }
-        vstrings.clear();
-       }
+    CTable<string*> STR (5,"string"); // CTable wskaźników na string
+    STR.setValue(new string ("a"),1);
+    cout<<*STR.getValue(1)<<endl;
+    int *tablica = new int [3];
+    tablica[0]=1;
+    tablica[1]=2;
+    CTable<int*> taboftabs (2,"tablicaTablic",tablica); //Ctable tablic int
+    int *pobrana= taboftabs.getValue(0);
+    cout<<pobrana[0]<<endl;
+    cout<<pobrana[1]<<endl;
+    CTable<string> A(1,"ssa","jup"); //Ctable string
+    try{
+    A.setValue("a",0);
+    A.setValue("b",1);}
+    catch (int w)
+    {if (w==-1) cout<<"Podany element jest spoza zakresu CTable"<<endl;}
+    try {
+        cout << A.getValue(3);
+    }
+    catch (int wyj){if (wyj==-1) cout<<"liczba spoza zakresy CTable"<<endl;}
+    Osoba a (10,"a");
+    CTable<Osoba> osobaTable (5,"osobaTab",a);
+    try{
+        cout<<osobaTable.getValue(0).toString()<<endl;
+    }
+    catch (int wyj){if (wyj==-1) cout<<"liczba spoza zakresy CTable"<<endl;}
 
     return 0;
     }
